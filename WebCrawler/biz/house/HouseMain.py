d7d08fa284c74f4c605642eb1eff15c997cb733b
diwu.sld <diwu.sld@taobao.com>
2017-01-06 11:25:03 +0800

    update

--- WebCrawler/biz/house/HouseMain.py
+++ WebCrawler/biz/house/HouseMain.py
@@ -1,14 +1,16 @@
 
 import os
 import  sys
-import biz.initialization as init
-import biz.house.XmcxMorePage as XP
 import datetime
 
 parent = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 parent = os.path.abspath(os.path.join(parent, os.pardir))
 sys.path.append(parent)
 print(sys.path)
+
+import biz.initialization as init
+import biz.house.XmcxMorePage as XP
+
 
 def begin(indexUrl, baseUrl):
     dbManager = init.getDbmanager('house')
